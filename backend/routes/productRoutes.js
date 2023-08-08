85f484e7b8decb76612367f27289b81800a2f60c
girok21 <hardeepsinha21@gmail.com>
2023-08-09 00:27:55 +0530

    added Error handling middleware & controllers for product routing

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,21 +1,9 @@
 import express from"express";
-import Product from '../models/productModel.js';
-
-import asyncHandler from "../middleware/asyncHandler.js";
+import { getProducts, getProductById } from "../controller/productController.js";
 
 const router = express.Router();
 
-router.get("/", asyncHandler(async(req,res)=>{
-    const products = await Product.find({});
-    res.json(products);
-}));
-
-router.get("/:id", asyncHandler(async(req,res)=>{
-    const product = await Product.findById(req.params.id);
-    if(!product){
-        res.status(404).json({message:'Product not found'});
-    }
-    res.json(product);
-}));
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
