e5f804be946d29f7e3949a8f50e2bda76a29d17a
girok21 <hardeepsinha21@gmail.com>
2023-08-08 23:59:27 +0530

    Initial frontend & backend setup with database connect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header"
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1>Welcome to GamerStop</h1>
+    <>
+      <Header />
+      {/* <main className='py-3' style={{padding:0}}> */}
+        <Container fluid style={{padding:0}}>
+          <Outlet />
+        </Container>
+      {/* </main> */}
+      <Footer />
+    </>
   )
 }
 
